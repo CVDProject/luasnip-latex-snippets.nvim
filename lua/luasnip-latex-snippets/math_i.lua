a7467e7f1f29a844bd2430bdfd3e9a931a2cedf3
cvdproject <106826078+CVDProject@users.noreply.github.com>
2024-09-28 19:33:13 +0200

    Update math_i.lua

--- lua/luasnip-latex-snippets/math_i.lua
+++ lua/luasnip-latex-snippets/math_i.lua
@@ -12,12 +12,12 @@ function M.retrieve(is_math)
   }) --[[@as function]]
 
   return {
-    parse_snippet({ trig = "lim", name = "limit" }, "\\lim_{${1:n} \\to ${2:\\infty}} "),
-    parse_snippet({ trig = "limsup", name = "limsup" }, "\\limsup_{${1:n} \\to ${2:\\infty}} "),
-    parse_snippet({ trig = "liminf", name = "liminf" }, "\\liminf_{${1:n} \\to ${2:\\infty}} "),
+    parse_snippet({ trig = "lim", name = "limit" }, "\\lim\\limits_{${1:n \\to ${2:\\infty}}} "),
+    parse_snippet({ trig = "limsup", name = "limsup" }, "\\limsup\\limits_{${1:n} \\to ${2:\\infty}} "),
+    parse_snippet({ trig = "liminf", name = "liminf" }, "\\liminf\\limits_{${1:n} \\to ${2:\\infty}} "),
 
-    parse_snippet({ trig = "overset", name = "overset" }, "\\overset{$1}${2:${TM_SELECTED_TEXT}} $0"),
-    parse_snippet({ trig = "underset", name = "underset" }, "\\underset{$1}${2:${TM_SELECTED_TEXT}} $0"),
+    parse_snippet({ trig = "overset", name = "overset" }, "\\overset{$2}${1:${TM_SELECTED_TEXT}} $0"),
+    parse_snippet({ trig = "underset", name = "underset" }, "\\underset{$2}${1:${TM_SELECTED_TEXT}} $0"),
     parse_snippet({ trig = "overbrace", name = "overbrace" }, "\\overbrace{$1:${TM_SELECTED_TEXT}}^{$2} $0"),
     parse_snippet({ trig = "underbrace", name = "underbrace" }, "\\underbrace{$1:${TM_SELECTED_TEXT}}_{$2} $0"),
     
