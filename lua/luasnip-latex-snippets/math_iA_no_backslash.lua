ffbbceffe0d6442650e7f05429775176e021354b
cvdproject <106826078+CVDProject@users.noreply.github.com>
2025-12-08 17:29:37 +0100

    Update math_iA_no_backslash.lua

--- lua/luasnip-latex-snippets/math_iA_no_backslash.lua
+++ lua/luasnip-latex-snippets/math_iA_no_backslash.lua
@@ -21,10 +21,10 @@ function M.retrieve(is_math)
 
     with_priority({ trig = "hat", name = "hat" }, "\\hat{$1}$0 "),
     with_priority({ trig = "bar", name = "bar" }, "\\overline{$1}$0 "),
+    with_priority({ trig = "und", name = "underline" }, "\\underline{$1}$0"),
     with_priority({ trig = "tld", name = "tilde" }, "\\tilde{$1}$0"),
 
     parse_snippet({ trig = "inf", name = "\\infty" }, "\\infty"),
-    parse_snippet({ trig = "inn", name = "in " }, "\\in "),
     parse_snippet({ trig = "SI", name = "SI" }, "\\SI{$1}{$2}"),
   }
 end
